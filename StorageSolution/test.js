b4c79c7f6d0e1fa143e0e4bf0ca1ec5781ce84a9
ElectrixFox <lukecphillips2006@gmail.com>
2024-07-21 11:27:25 +0100

    Working on the outputting
    Now has the ability to search for records on a given date and return the IDs of them. Currently the system is being tested to be able to get all records on a given date in order to use this information to be able to output them to the screen.

--- StorageSolution/test.js
+++ StorageSolution/test.js
@@ -5,13 +5,16 @@ STITCHLOG (stlogID, wipID, status, date, ...)
 status: Stitched (S), Finished (F), F Finished (FFO), New Start (N)
 */
 
+// converts a string to an integer
+function toInt(str) { return parseInt(str, 10); }
+
 class StitchLog
     {
     constructor(logID, wipID, stDate, stStatus)
         {
-        if(logID === null)
+        if(logID[0] === null)
             {
-            this = somefunctiontoloadstitchrecords();   // this will come from the JSON file
+            // this = somefunctiontoloadstitchrecords();   // this will come from the JSON file
             }
         
         this.logID = logID;
@@ -20,9 +23,8 @@ class StitchLog
         this.stStatus = stStatus;
         
         this.recNo = logID.length;
+        this.SortLogByDate();
         }
-
-    get recNo() { return this.recNo; }
 
     findRecord(logID)
     {
@@ -77,9 +79,9 @@ class StitchLog
     const len = this.recNo;
 
     for (let i = 0; i < len; i++)
-        for (let j = 0; j < len; j++)
-            if(DateComp(this.stDate[i], this.stDate[j]) === -1)
-                this.SwapRecords(i, j, 1);    
+        for (let j = 0; j < (len - i - 1); j++)
+            if(DateComp(this.stDate[j], this.stDate[j + 1]) === 1)
+                this.SwapRecords(j, j + 1, 1);    
     }
 
     findNextNumber()
@@ -128,11 +130,37 @@ class StitchLog
 
     this.recNo = logID.length - 1;
     }
+
+    // returns IDs of records made on a date
+    findRecordsOnDate(date)
+    {
+    let IDs = [];
+    this.SortLogByDate();
+
+    // Improvement: could probably turn this into a binary search to the first occurance of the date
+    // Improvement: possible improvement, stop adding when the date changes after adding has started
+
+    for (let i = 0; i < this.recNo; i++)    // loop through all records
+        {
+        if (DateComp(this.stDate[i], date) === 0)   // if the record is on the same date as the input date
+            {
+            IDs.push(this.logID[i]);    // add the ID of the record to the ID array
+            }
+        }
+    
+    return IDs;
+    }
 }
 
-function CreateHTMLStitchTable()
+function LoadStitchLog()
 {
-
+let log = new StitchLog(
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    [2, 2, 3, 5, 1, 2, 3, 3, 5, 1],
+    ["02/01/2024", "10/01/2024", "09/01/2024", "04/01/2024", "05/01/2024", "01/01/2024", "10/01/2024", "08/01/2024", "06/01/2024", "06/01/2024"],
+    ["N", "S", "FFO", "F", "N", "N", "S", "N", "F", "F"]
+);
+return log;
 }
 
 // -1 is dat1 before dat2, 1 is dat1 after dat2, 0 is when they are the same, -2 is error 
@@ -157,43 +185,60 @@ else if (idat1 === idat2)   // if they are the same date return 0
 return -2;  // return -2 for an error
 }
 
+// gets the year part from the date (in the format dd/mm/yyyy)
+function getDateYear(dat) { return toInt(dat.substring(6, 10)); }
+
 function createTable()
 {
-const stTable = new StitchLog();
-const records = [];
+const stTable = new StitchLog();    // creating the record log
 const recNo = stTable.recNo();
-const wipNo = 0;
 let table = [];
 let daysinmo = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];    // number of days in each month
 
-stTable.SortLogByDate();    // sorting the records
+const mindat = stTable.stDate[0];  // minimum date for the log table
+const mindatyr = getDateYear(mindat);   // gets the year of the minimum date
+const maxdat = stTable.stDate[recNo - 1];  // maximum date for the log table
+const nyrs = getDateYear(maxdat) - mindatyr;   // gets number of years between the two dates
 
-let tmpMinDat = stTable.stDate[0];  // temporary minimum date for the record
-let tmpMaxDat = stTable.stDate[recNo - 1];  // temporary maximum date for the record
-const nyrs = stTable.getRecordYear(stTable.logID[recNo - 1]) - stTable.getRecordYear(stTable.logID[0]);   // gets number of years between the two dates
+// could be an idea to rework the creation to populate the array as it is being created with the appropriate records
 
 for (let yr = 0; yr <= nyrs; yr++)
     {
-    if((stTable.getRecordYear(stTable.logID[0] + yr) % 4) === 0)    // if it is a leap year
-        daysinmo[2] = 29;   // set Feb to having 29 days
-    else    // else if it is not a leap year
-        daysinmo[2] = 28;   // ser Feb to having 28 days
+    const daysinyear = 365 + (((toInt(mindatyr) + yr) % 4) === 0);  // the number of days in the year (+1 if it is a leap year)
+    
+    // create new table for year
+    table = [];
 
-
-    for (let mon = 0; mon < 12; mon++) 
+    // adds an entry for each day
+    for (let day = 0; day < daysinyear; day++)
         {
-        for (let day = 0; day < daysinmo[mon]; day++) 
-            {
-            table.
-            }
+        table[day] = [];
         }
     }
+}
 
-for (let i = 0; i <= nyrs; i++)
+function ReplaceSection(str, start, end, item)
+{
+return str.slice(start - 1, end) + item + str.slice(end + 1);
+}
+
+function CreateHTMLStitchTable()
+{
+let stLog = LoadStitchLog();  // gets the log
+const nowips = 2;
+let tmpdat = "01/01/2024";
+
+for (let i = 1; i < 11; i++)
     {
-    table[i] = [];
-
+    let recIDs = stLog.findRecordsOnDate(tmpdat); // finds all the records on the given date
+    console.log("\n\nDate: %s", tmpdat);
+    for (let j = 0; j < recIDs.length; j++)
+        {
+        console.log("\nEntry %d: %s", j + 1, stLog.stStatus[stLog.findRecord(recIDs[j])]);
+        }
+    
+    const dbfore = (toInt(tmpdat[1]) + 1).toString(10).padStart(2, "0")
+    tmpdat = ReplaceSection(tmpdat, 0, 1, dbfore);
+    //tmpdat = tmpdat.slice(-1, 1) + (toInt(tmpdat[1]) + 1).toString(10).padStart(2, "0") + tmpdat.slice(1 + 1);
     }
-
-
 }
