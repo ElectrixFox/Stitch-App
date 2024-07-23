7e5becf7ed3421e0dec4fb53305187b63648ca40
ElectrixFox <lukecphillips2006@gmail.com>
2024-07-23 11:09:12 +0100

    Reorganising code
    The code is being reorganised because there is some redundancy in the main file so I am trying to reduce it since the main file is so big. It would be useful to be able to split the main file into smaller files however this will require more work on understanding js.

--- public/main.js
+++ public/main.js
@@ -11,8 +11,99 @@ Bugs:
 - Ensure that WIP records cannot be made on the start date and end date
 */
 
+//#region Utilities
+
 // converts a string to an integer
 function toInt(str) { return parseInt(str, 10); }
+
+// -1 is dat1 before dat2, 1 is dat1 after dat2, 0 is when they are the same, -2 is error 
+function DateComp(dat1, dat2)
+{
+let CalcNumericalDate = (dat) => { return ((10000) * toInt(dat.substring(6, 10)) + (100) * toInt(dat.substring(3, 5)) + toInt(dat.substring(0, 2))); }; // function to calculate the numerical date for comparison
+// gets the dates in the form yyyymmdd
+let idat1 = CalcNumericalDate(dat1);
+let idat2 = CalcNumericalDate(dat2);
+
+if(idat1 < idat2)   // if the first date is before the 2nd date return -1
+    {
+    return -1;
+    }
+if(idat1 > idat2)    // if the first date is after the 2nd date return 1
+    {
+    return 1;
+    }
+else if (idat1 === idat2)   // if they are the same date return 0
+    {
+    return 0;
+    }
+return -2;  // return -2 for an error
+}
+
+// gets the year part from the date (in the format dd/mm/yyyy)
+function getDateYear(dat) { return toInt(dat.substring(6, 10)); }
+
+function getScheduleLoc(dat)
+{
+let daysinmo = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];    // number of days in each month
+let month = dat.substring(3, 5);    // gets month part of the date
+const day = dat.substring(0, 2);    // gets day part of the date
+let res = day;
+
+if ((toInt(getDateYear(dat)) % 4) === 0)    // if it is a leap year
+    daysinmo[1] = 29;   // set Feb to have 29 days
+
+for (let i = 0; i < month; i++) // loops through months until month of given date
+    {
+    res += daysinmo[month]; // adds days in previous month to the result
+    }
+
+return res;
+}
+
+//#endregion
+
+//#region HTML utilities
+
+function RemoveHTMLElementChildren(eleID, eleIDstoignore = null)
+{
+const ele = document.getElementById(eleID); // gets the ID of the element to clear
+let toremove = [];  // elements to remove
+let cont = 1;
+
+if(eleIDstoignore === null) // if there are no IDs to ignore
+    {
+    while (ele.childElementCount > 0)  // while there are still child elements
+        {
+        const child = ele.childNodes[0];    // select the first child element 
+        ele.removeChild(child);    // remove the child
+        }
+    }
+else
+    {
+    for (let i = 0; i < ele.childNodes.length; i++) // loop through all child nodes
+        {
+        for (let j = 0; j < eleIDstoignore.length; j++)  // loop through all element IDs to ignore
+            {
+            if (ele.childNodes[i].id === eleIDstoignore[j])  // if there is a child node with an ID to ignore
+                {
+                cont = 0;   // don't continue with the removal
+                break;  // exit the search
+                }
+            }
+        if(cont === 1)  // if should continue with the removal
+            {
+            toremove.push(ele.childNodes[i]);   // add the child to the list
+            }
+        cont = 1;   // assume the next one isn't in the ignore list either
+        }
+    while(toremove.length > 0)  // while there are elements to remove
+        {
+        ele.removeChild(toremove.pop());
+        }
+    }
+}
+
+//#endregion
 
 class WipsTable
     {
@@ -294,27 +385,6 @@ class StitchLog
 
 }
 
-function setStitchLogFromJSON(jsonData)
-{
-let strids = [];
-let wipids = [];
-let recdats = [];
-let recstats = [];
-
-for(let i = 0; i < jsonData.length; i++)    // loop through all of the entries
-    {
-    let item = jsonData[i]; // getting each json entry
-
-    // adding the new item
-    strids.push(item['strecID']);
-    wipids.push(item['wipID']);
-    recdats.push(item['recDate']);
-    recstats.push(item['recStatus']);
-    }
-
-return new StitchLog(strids, wipids, recdats, recstats);    // creating the stitch log
-}
-
 function setWipTableFromJSON(jsonData)
 {
 let new_wipID = []
@@ -346,78 +416,101 @@ for(let i = 0; i < jsonData.length; i++)    // loop through all of the entries
 return new WipsTable(new_wipID, new_wipName, new_designer, new_stDate, new_finDate, new_stitchcount, new_fabric, new_floss, new_notes);    // creating the wip table
 }
 
-function LoadStitchLog()
+function setStitchLogFromJSON(jsonData)
 {
-let log = new StitchLog(
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    [2, 2, 3, 5, 1, 2, 3, 3, 5, 1],
-    ["02/01/2024", "10/01/2024", "09/01/2024", "04/01/2024", "05/01/2024", "01/01/2024", "10/01/2024", "08/01/2024", "06/01/2024", "06/01/2024"],
-    ["N", "S", "FFO", "F", "N", "N", "S", "N", "F", "F"]
-);
+let strids = [];
+let wipids = [];
+let recdats = [];
+let recstats = [];
 
-return log;
+for(let i = 0; i < jsonData.length; i++)    // loop through all of the entries
+    {
+    let item = jsonData[i]; // getting each json entry
+
+    // adding the new item
+    strids.push(item['strecID']);
+    wipids.push(item['wipID']);
+    recdats.push(item['recDate']);
+    recstats.push(item['recStatus']);
+    }
+
+return new StitchLog(strids, wipids, recdats, recstats);    // creating the stitch log
 }
 
-function LoadWipTable()
+function SaveJSONFile(filename, data)
 {
-let wips = new WipsTable(
-    [1, 2, 3, 4, 5],
-    ["A", "B", "C", "D", "E"],
-    ["0", "0", "0", "0", "0"],
-    ["01/01/2024", "05/01/2024", "06/04/2024", "05/03/2024", "11/11/2024"],
-    ["15/02/2024", "09/03/2024", "19/08/2024", "21/04/2024", "24/12/2024"],
-    [60, 120, 150, 190, 100],
-    ["", "", "", "", ""],
-    ["", "", "", "", ""],
-    ["", "", "", "", ""]
-);
+const blob = new Blob([JSON.stringify(data, null, 2)], { // creates json blob with the type of json 
+    type: "application/json",
+});
 
-return wips;
+const url = 'http://localhost:8000/write-file?name=' + filename + '.json'; // url to upload to
+
+const formdata = new FormData();    // creates the new form to attatch the data to
+formdata.append('file', blob, filename + '.json'); // adds the data to the form
+
+fetch(url, {
+    method: 'POST',
+    body: formdata
+})
+.then(response => { // sorting the responce
+    if (!response.ok)   // if the responce is bad
+        throw new Error("Failed to upload file");   // give error message 
+    return response.json(); // return the responce
+})
+.then(data => { // sorting the data
+    console.log('File uploaded successfully:', data);
+})
+.catch(error => {   // catching an error
+    console.error('Error uploading file:', error);
+});
 }
 
-function CalcNumericalDate(dat) { return ((10000) * toInt(dat.substring(6, 10)) + (100) * toInt(dat.substring(3, 5)) + toInt(dat.substring(0, 2))); }
-
-// -1 is dat1 before dat2, 1 is dat1 after dat2, 0 is when they are the same, -2 is error 
-function DateComp(dat1, dat2)
+function LoadJSONFile(filename)
 {
-// gets the dates in the form yyyymmdd
-let idat1 = CalcNumericalDate(dat1);
-let idat2 = CalcNumericalDate(dat2);
+const url = 'http://localhost:8000/read-file?name=' + filename + '.json'; // url to load from
 
-if(idat1 < idat2)   // if the first date is before the 2nd date return -1
-    {
-    return -1;
-    }
-if(idat1 > idat2)    // if the first date is after the 2nd date return 1
-    {
-    return 1;
-    }
-else if (idat1 === idat2)   // if they are the same date return 0
-    {
-    return 0;
-    }
-return -2;  // return -2 for an error
-}
-
-// gets the year part from the date (in the format dd/mm/yyyy)
-function getDateYear(dat) { return toInt(dat.substring(6, 10)); }
-
-function getScheduleLoc(dat)
-{
-let daysinmo = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];    // number of days in each month
-let month = dat.substring(3, 5);    // gets month part of the date
-const day = dat.substring(0, 2);    // gets day part of the date
-let res = day;
-
-if ((toInt(getDateYear(dat)) % 4) === 0)    // if it is a leap year
-    daysinmo[1] = 29;   // set Feb to have 29 days
-
-for (let i = 0; i < month; i++) // loops through months until month of given date
-    {
-    res += daysinmo[month]; // adds days in previous month to the result
-    }
+const res = fetch(url)  // setting the result to be the fetched data
+.then(response => { // sorting the responce
+    if (!response.ok)   // if the responce is bad
+        throw new Error("Failed to load file");   // give error message 
+    return response.json(); // return the responce
+})
+.then(data => { // sorting the data
+    console.log('File loaded successfully:', data);
+    console.log(data[0]);
+    return data;
+})
+.catch(error => {   // catching an error
+    console.error('Error loading file:', error);
+});
 
 return res;
+}
+
+function SaveWipTableFile(wiptable)
+{
+const wiptab = wiptable.getAsJSON();
+console.log(wiptab);
+SaveJSONFile('wiptable', wiptab);
+}
+
+async function LoadWipTableFile()
+{
+let data = await LoadJSONFile('wiptable'); 
+return setWipTableFromJSON(data);
+}
+
+function SaveStitchLogFile(stitchlog)
+{
+const stlog = stitchlog.getAsJSON();
+console.log(stlog);
+SaveJSONFile('stitchlog', stlog);
+}
+
+async function LoadStitchLogFile()
+{
+let data = await LoadJSONFile('stitchlog'); 
+return setStitchLogFromJSON(data);
 }
 
 function createTable()
@@ -498,9 +591,9 @@ for (let i = 0; i < logrecords.length; i++)
     }
 }
 
-function CreateHTMLWipTable(wipid = 0)
+async function CreateHTMLWipTable(wipid = 0)
 {
-let wipstable = LoadWipTable();
+let wipstable = await LoadWipTableFile();
 const wiploc = wipstable.findWip(wipid);
 
 let createBlock = (intable, title, content) => {
@@ -510,6 +603,7 @@ let createBlock = (intable, title, content) => {
 
     const bodcell = document.createElement('td');   // creating content cell
     bodcell.textContent = content;  // setting the text content
+    bodcell.contentEditable = true;
 
     nrow.appendChild(headcell);
     nrow.appendChild(bodcell);
@@ -582,29 +676,9 @@ if(title !== '')    // if there is a title meaning it is the header cell
 return span;   // returns 1 so that the span can be updated
 }
 
-function InitialiseStitchLogTable()
+function SaveRecordLog(stitchLog)
 {
-let stLog = LoadStitchLog();  // gets the log
-const nowips = 2;
-let tmpdat = "01/01/2024";
-
-for (let i = 1; i < 11; i++)
-    {
-    let recIDs = stLog.findRecordsOnDate(tmpdat); // finds all the records on the given date
-    console.log("\n\nDate: %s", tmpdat);
-    for (let j = 0; j < recIDs.length; j++)
-        {
-        console.log("\nEntry %d: %s", j + 1, stLog.recStatus[stLog.findRecord(recIDs[j])]);
-        }
-    
-    const dbfore = (toInt(tmpdat[1]) + 1).toString(10).padStart(2, "0");
-    tmpdat = ReplaceSection(tmpdat, 0, 1, dbfore);
-    }
-}
-
-function SaveRecordLog()
-{
-const stLog = LoadStitchLog().getAsJSON();
+const stLog = stitchLog.getAsJSON();
 
 const blob = new Blob([JSON.stringify(stLog, null, 2)], { // creates json blob with the type of json 
     type: "application/json",
@@ -656,73 +730,8 @@ const res = fetch(url)  // setting the result to be the fetched data
 return res;
 }
 
-function SaveWipTableFile(wiptable)
-{
-const wiptab = wiptable.getAsJSON();
-console.log(wiptab);
-SaveJSONFile('wiptable', wiptab);
-}
-
-async function LoadWipTableFile()
-{
-let data = await LoadJSONFile('wiptable'); 
-return setWipTableFromJSON(data);
-}
-
-function SaveJSONFile(filename, data)
-{
-const blob = new Blob([JSON.stringify(data, null, 2)], { // creates json blob with the type of json 
-    type: "application/json",
-});
-
-const url = 'http://localhost:8000/write-file?name=' + filename + '.json'; // url to upload to
-
-const formdata = new FormData();    // creates the new form to attatch the data to
-formdata.append('file', blob, filename + '.json'); // adds the data to the form
-
-fetch(url, {
-    method: 'POST',
-    body: formdata
-})
-.then(response => { // sorting the responce
-    if (!response.ok)   // if the responce is bad
-        throw new Error("Failed to upload file");   // give error message 
-    return response.json(); // return the responce
-})
-.then(data => { // sorting the data
-    console.log('File uploaded successfully:', data);
-})
-.catch(error => {   // catching an error
-    console.error('Error uploading file:', error);
-});
-}
-
-function LoadJSONFile(filename)
-{
-const url = 'http://localhost:8000/read-file?name=' + filename + '.json'; // url to load from
-
-const res = fetch(url)  // setting the result to be the fetched data
-.then(response => { // sorting the responce
-    if (!response.ok)   // if the responce is bad
-        throw new Error("Failed to load file");   // give error message 
-    return response.json(); // return the responce
-})
-.then(data => { // sorting the data
-    console.log('File loaded successfully:', data);
-    console.log(data[0]);
-    return data;
-})
-.catch(error => {   // catching an error
-    console.error('Error loading file:', error);
-});
-
-return res;
-}
-
 async function CreateWipList()
 {
-// <button class="wiplist-content" id="wiplist-newwip" onclick="CreateNewWip()">New Wip</button>
-
 let wiptable = await LoadWipTableFile();    // loads the wip table in
 const wiplist = document.getElementById("wiplist-content"); // gets the wip list element
 const btn = document.getElementById("wiplist-newwip");  // gets the new wip button
@@ -742,42 +751,6 @@ for (let i = 0; i < wiptable.nowips; i++)   // loop through all of the wips
     {
     newitem(wiptable.wipName[i]);   // adds each wip to the list
     }
-
-}
-
-function RemoveHTMLElementChildren(eleID, eleIDstoignore = null)
-{
-const ele = document.getElementById(eleID); // gets the ID of the element to clear
-let toremove = [];  // elements to remove
-let cont = 1;
-
-if(eleIDstoignore === null) // if there are no IDs to ignore
-    {
-    while (ele.childElementCount > removeat)  // while there are still child elements
-        {
-        const child = ele.childNodes[removeat];    // select the first child element 
-        ele.removeChild(child);    // remove the child
-        }
-    }
-else
-    {
-    for (let i = 0; i < ele.childNodes.length; i++) // loop through all child nodes
-        {
-        for (let j = 0; j < eleIDstoignore.length; j++)  // loop through all element IDs to ignore
-            {
-            if (ele.childNodes[i].id === eleIDstoignore[j])  // if there is a child node with an ID to ignore
-                {
-                cont = 0;   // don't continue with the removal
-                break;  // exit the search
-                }
-            }
-        if(cont === 1)  // if should continue with the removal
-            {
-            toremove.push(ele.childNodes[i]);   // add the child to the list
-            }
-        cont = 1;   // assume the next one isn't in the ignore list either
-        }
-    }
 }
 
 async function CreateNewWip()
@@ -786,14 +759,7 @@ const wiptable = await LoadWipTableFile();    // loads the wip table
 const container = document.getElementById("container_wiplist");    // loads the wip table
 
 RemoveHTMLElementChildren('wipdetstab');    // removes all the table columns for it to be rebuilt
-RemoveHTMLElementChildren('wiplist-content', 'wiplist-newwip');    // removes all the table columns for it to be rebuilt
-
-/* while (otab.childElementCount > 0)
-    {
-    const child = otab.childNodes[0];
-    console.log(child);
-    otab.removeChild(child);
-    } */
+RemoveHTMLElementChildren('wiplist-content', ['wiplist-newwip']);    // removes all the options in the drop down menu apart from the button
 
 const nID = wiptable.AddWip(-1, "", "", "", "", "", "", "", "");    // adds the new wip
 
