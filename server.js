2e7bd8b149c9f8482ec371ec6d76d9de5f52c5b8
ElectrixFox <lukecphillips2006@gmail.com>
2024-07-31 11:03:33 +0100

    Small configuration update
    The cors settings have been updated to fix one or two of the problems but there are still issues when trying to connect from another device to the server hosted on the computer. Additionally, if the server is hosted from the computer then the hostname parameter should be removed from the listen function in order to set the IP of the system to be the destination.

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express'); // requires the express package
 const path = require('path');   // requires the path package
+const cors = require('cors');
 
 const port = 8000;
 const hostname = 'localhost';
 
 const app = express();
 
+app.use(cors({ origin: true, credentials: true })); // stops the cors issues when connecting
 app.use(express.static('public')); // the location of all the static files
 app.set('view engine', 'ejs');  // setting the viewing engine as ejs
 
@@ -29,4 +31,4 @@ app.use('/wipview', wipRouter);   // use the wip router
 app.use('/wipoverview', overviewRouter);   // use the overview router
 app.use('/', fileRouter);   // use the file router
 
-app.listen(port, hostname);
+app.listen(port, hostname);   // removed the host name to be able to go to the site on external devices
